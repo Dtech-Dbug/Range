8fb673f6a8cfe98e8c8dd5c4dcd48e16314fbbc7
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-10-27 22:55:32 +0530

    fixed parameter overriding issue

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ function Range(start = 0, limit, step = 1) {
   // check args vector
   if (arguments.length == 1) {
     limit = arguments[0];
+    start = 0;
   }
-  console.log(limit);
 
   for (i = start; i < limit; i = i + step) {
-    console.log(arguments[i]);
+    console.log(i);
   }
 }
 
