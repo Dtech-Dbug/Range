ef9b3c9d3030e1b4ad6638f69d8802ac958eb8c5
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-10-27 22:52:04 +0530

    added arg vector check
    if one arg is passed the arg is set to limit instead of start parameter

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 function Range(start = 0, limit, step = 1) {
+  // check args vector
+  if (arguments.length == 1) {
+    limit = arguments[0];
+  }
+  console.log(limit);
+
   for (i = start; i < limit; i = i + step) {
-    console.log(i);
+    console.log(arguments[i]);
   }
 }
 
-Range(0, 10, 2);
+Range(10);
