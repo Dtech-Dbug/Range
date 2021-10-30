bced20ab0c2eb29ddc5027f3fab8a914bd6f3e81
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-10-30 20:25:30 +0530

    update : added negative step counts

--- src/index.ts
+++ src/index.ts
@@ -2,17 +2,24 @@ function range(start: number = 0, limit: number, step: number = 1) {
   let iterators: number[] = [];
 
   // TODO : check args vector
-
+  console.log(start, limit, step);
   if (arguments.length == 1) {
     limit = arguments[0];
     start = 0;
   }
 
-  for (let i: number = start; i < limit; i = i + step) {
-    iterators.push(i);
+  if (start < limit) {
+    for (let i: number = start; i < limit; i = i + step) {
+      iterators.push(i);
+    }
+  } else {
+    for (let i: number = start; i > limit; i = i + step) {
+      iterators.push(i);
+    }
   }
 
   return iterators;
 }
 
+console.log(range(10, 1, -2));
 module.exports = range;
